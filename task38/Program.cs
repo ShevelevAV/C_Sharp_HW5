fb4b95bf1fc4591beb64a66b29674e75ab374900
Aleksandr Shevelev <leksevi@icloud.com>
2022-12-19 20:20:50 +0300

    add task 38

---
+++ task38/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76*/
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] temp = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        temp[i] = new Random().Next(min, max);
+    }
+    return temp;
+}
+void PrintArray(int[] pa)
+{
+    for (int i = 0; i < pa.Length; i++)
+    {
+        Console.Write($"{pa[i]} ");
+    }
+}
+void DiffMinMax(int[] arr)
+{
+    int i = 0;
+    int max = arr[i];
+    int min = arr[i];
+    while (i < arr.Length)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+        i++;
+    }
+    Console.Write($"{max} - {min} = {max - min} ");
+
+}
+
+int[] array = GetArray(5, 1, 100);
+PrintArray(array);
+Console.WriteLine();
+DiffMinMax(array);
